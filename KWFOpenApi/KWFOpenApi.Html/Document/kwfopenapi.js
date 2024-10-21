0966ca0486f68124000449ebe6061a51517c62ba
Joao Morgado <joaomorgado0@gmail.com>
2024-10-21 15:09:45 +0100

    update request when changing media type, add endpoint and method info

--- KWFOpenApi/KWFOpenApi.Html/Document/kwfopenapi.js
+++ KWFOpenApi/KWFOpenApi.Html/Document/kwfopenapi.js
@@ -10,7 +10,7 @@ var CurrentSelectedMetadata =
     ReqMediaTypes: {},
     ReqSamples: {},
     RespSamples: {},
-    ReqObjRef: undefined,
+    ReqObjRef: {},
     EndpointId: undefined,
     EndpointRoute: undefined,
     EndpointMethod: undefined
@@ -18,7 +18,6 @@ var CurrentSelectedMetadata =
 
 //request box text states for all endpoints and media types
 var LoadedRequests = {};
-
 
 //show or hide endpoints from group
 function ExpandEndpointGroup(group_div, endpoint_div_id) {
@@ -38,18 +37,10 @@ function ExpandEndpointGroup(group_div, endpoint_div_id) {
 
 function SelectEndpoint(endpoint_id) {
     var requestBox = document.getElementById("request_box");
+    var reqRefBody = document.getElementById("req-obj-ref-item");
     var requestSelectedMediaSelect = document.getElementById("request_selected_media");
     //save previous state - CurrentSelectedMetadata should be previous endpoint
-    if (CurrentSelectedMetadata.EndpointId !== null && CurrentSelectedMetadata.EndpointId !== undefined) {
-        var prevReqState = LoadedRequests[CurrentSelectedMetadata.EndpointId];
-        if (prevReqState !== null && prevReqState !== undefined) {
-            LoadedRequests[CurrentSelectedMetadata.EndpointId].body[CurrentSelectedMetadata.ReqSelectedMedia] = requestBox.value;
-            LoadedRequests[CurrentSelectedMetadata.EndpointId].media = CurrentSelectedMetadata.ReqSelectedMedia;
-        }
-    }
-
-    //get req state for endpoint (if any)
-    var currentRequest = LoadedRequests[endpoint_id];
+    SavePreviousRequestState(requestBox);
 
     //load endpoint to current state
     CurrentSelectedMetadata.EndpointId = endpoint_id;
@@ -58,28 +49,28 @@ function SelectEndpoint(endpoint_id) {
     CurrentSelectedMetadata.ReqSamples = {};
     CurrentSelectedMetadata.ReqMediaTypes = {};
     CurrentSelectedMetadata.RespSamples = {};
+    CurrentSelectedMetadata.ReqObjRef = {};
 
-    var req_obj_refs = document.getElementsByName("request_object_" + endpoint_id);
+    //get req state for endpoint (if any)
+    var currentRequest = LoadedRequests[endpoint_id];
+    if (currentRequest === null || currentRequest === undefined) {
+        CurrentSelectedMetadata.ReqSelectedMedia = DefaultSelectedMedia;
+    }
+    else {
+        CurrentSelectedMetadata.ReqSelectedMedia = currentRequest.media;
+    }
+
+    var reqSamples = document.getElementsByName("request_sample_" + endpoint_id + "[]");
     //only map requests if conditions met
-    if (CurrentSelectedMetadata.EndpointMethod !== "GET" && CurrentSelectedMetadata.EndpointMethod !== "DELETE" && req_obj_refs.length > 0) {
-        CurrentSelectedMetadata.ReqObjRef = req_obj_refs[0].getAttribute("value");
-
-        if (currentRequest === null || currentRequest === undefined) {
-            CurrentSelectedMetadata.ReqSelectedMedia = DefaultSelectedMedia;
-        }
-        else {
-            CurrentSelectedMetadata.ReqSelectedMedia = currentRequest.media;
-        }
-
-        //handle requests
-        var reqSamples = document.getElementsByName("request_sample_" + endpoint_id + "[]");
+    if (CurrentSelectedMetadata.EndpointMethod !== "GET" && CurrentSelectedMetadata.EndpointMethod !== "DELETE") {
         if (reqSamples.length > 0) {
-
+            //handle requests
             //save request samples to current state
             reqSamples.forEach(reqSample => {
                 var sampleKey = reqSample.getAttribute("kwf-media-type");
                 CurrentSelectedMetadata.ReqSamples[sampleKey] = reqSample.getAttribute("value");
                 CurrentSelectedMetadata.ReqMediaTypes[sampleKey] = reqSample.getAttribute("kwf-media-type-name");
+                CurrentSelectedMetadata.ReqObjRef[sampleKey] = reqSample.getAttribute("kwf-obj_ref");
             });
 
             //save first request from sample to history
@@ -104,14 +95,32 @@ function SelectEndpoint(endpoint_id) {
                 LoadedRequests[endpoint_id].body[sampleReqKey] = sampleRequest
             }
         }
+        else {
+            //save first request to history when no sample available
+            CurrentSelectedMetadata.ReqMediaTypes[CurrentSelectedMetadata.ReqSelectedMedia] = "application/json"; //maybe should be plain text?
+            if (currentRequest === null || currentRequest === undefined) {
+                currentRequest = {
+                    body: {},
+                    media: CurrentSelectedMetadata.ReqSelectedMedia
+                };
+                LoadedRequests[endpoint_id] = currentRequest;
+            }
+
+            if (currentRequest.body[CurrentSelectedMetadata.ReqSelectedMedia] === null || currentRequest.body[CurrentSelectedMetadata.ReqSelectedMedia] === undefined) {
+                var sampleReqKey = DefaultSelectedMedia;
+                var sampleRequest = "\"\"";
+                LoadedRequests[endpoint_id].media = sampleReqKey;
+                LoadedRequests[endpoint_id].body[sampleReqKey] = sampleRequest
+            }
+        }
 
         //get req body for current media type selected
         //check if box is disable, enable it
         requestBox.removeAttribute("readonly");
         requestBox.classList.remove("textbox-readonly");
         requestBox.value = LoadedRequests[CurrentSelectedMetadata.EndpointId]?.body[CurrentSelectedMetadata.ReqSelectedMedia];
-        //var selectedMediaTypeName = CurrentSelectedMetadata.ReqMediaTypes[CurrentSelectedMetadata.ReqSelectedMedia];
-        //requestSelectedMediaDiv.innerText = selectedMediaTypeName;
+        reqRefBody.innerHTML = CurrentSelectedMetadata.ReqObjRef[CurrentSelectedMetadata.ReqSelectedMedia];
+        reqRefBody.setAttribute("kwf-req-obj-ref", CurrentSelectedMetadata.ReqObjRef[CurrentSelectedMetadata.ReqSelectedMedia]);
 
         var mediaTypesAvailable = Object.keys(CurrentSelectedMetadata.ReqMediaTypes);
         mediaTypesAvailable.forEach(type => {
@@ -128,9 +137,13 @@ function SelectEndpoint(endpoint_id) {
         requestBox.value = "";
         requestBox.classList.add("textbox-readonly");
         requestBox.setAttribute("readonly", true);
-        //requestSelectedMediaDiv.innerText = "";
-        requestSelectedMediaDiv.innerHTML = "";
+        requestSelectedMediaSelect.innerHTML = "";
+        reqRefBody.innerHTML = "";
+        reqRefBody.removeAttribute("kwf-req-obj-ref");
     }
+
+    var endpointDataDiv = document.getElementById("api-selected-endpoint-data");
+    endpointDataDiv.innerHTML = "[" + CurrentSelectedMetadata.EndpointMethod + "] => " + CurrentSelectedMetadata.EndpointRoute
 }
 
 function ReloadRequestSample() {
@@ -145,6 +158,63 @@ function ReloadRequestSample() {
     }
 }
 
-function ChangeReqMediaType(mediaType) {
+function ChangeReqMediaType(mediaTypeSelect) {
+    var mediaType = mediaTypeSelect.value;
 
+    //nothing selected or same media type, no change
+    if (CurrentSelectedMetadata === null ||
+        CurrentSelectedMetadata === undefined ||
+        mediaType == CurrentSelectedMetadata.ReqSelectedMedia) {
+            return;
+    }
+
+    //get and delete have no request body. no change
+    if (CurrentSelectedMetadata.EndpointMethod === null ||
+        CurrentSelectedMetadata.EndpointMethod === undefined ||
+        CurrentSelectedMetadata.EndpointMethod === "GET" ||
+        CurrentSelectedMetadata.EndpointMethod === "DELETE") {
+            return;
+    }
+
+    var requestBox = document.getElementById("request_box");
+    // save to request states
+    SavePreviousRequestState(requestBox);
+
+    CurrentSelectedMetadata.ReqSelectedMedia = mediaType;
+    var requestsForEndpoint = LoadedRequests[CurrentSelectedMetadata.EndpointId];
+    if (requestsForEndpoint === null || requestsForEndpoint === undefined) {
+        LoadedRequests[CurrentSelectedMetadata.EndpointId] = {
+            media: mediaType,
+            body: {}
+        }
+
+        requestsForEndpoint = LoadedRequests[CurrentSelectedMetadata.EndpointId];
+    }
+
+    var stateForMediaType = requestsForEndpoint.body[mediaType];
+    if (stateForMediaType === null || stateForMediaType === undefined) {
+        var sampleReq = CurrentSelectedMetadata.ReqSamples[mediaType];
+
+        if (sampleReq === null || sampleReq === undefined) {
+            sampleReq = "\"\"";
+        }
+
+        LoadedRequests[CurrentSelectedMetadata.EndpointId].body[mediaType] = sampleReq;
+    }
+
+    reqRefBody.innerHTML = CurrentSelectedMetadata.ReqObjRef[mediaType];
+    reqRefBody.setAttribute("kwf-req-obj-ref", CurrentSelectedMetadata.ReqObjRef[mediaType]);
+    requestBox.removeAttribute("readonly");
+    requestBox.classList.remove("textbox-readonly");
+    requestBox.value = LoadedRequests[CurrentSelectedMetadata.EndpointId]?.body[mediaType];
+}
+
+function SavePreviousRequestState(requestBox) {
+    if (CurrentSelectedMetadata.EndpointId !== null && CurrentSelectedMetadata.EndpointId !== undefined) {
+        var prevReqState = LoadedRequests[CurrentSelectedMetadata.EndpointId];
+        if (prevReqState !== null && prevReqState !== undefined) {
+            LoadedRequests[CurrentSelectedMetadata.EndpointId].body[CurrentSelectedMetadata.ReqSelectedMedia] = requestBox.value;
+            LoadedRequests[CurrentSelectedMetadata.EndpointId].media = CurrentSelectedMetadata.ReqSelectedMedia;
+        }
+    }
 }
